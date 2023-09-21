4c4287334ca85a422eccd9daa14ad364bf08bbaf
Rhinoj0sa <rhinojosaram@gmail.com>
2023-09-20 21:12:17 -0600

    Up to section2 lesson 13

--- src/08-inferring-schema-types/types.ts
+++ src/08-inferring-schema-types/types.ts
@@ -1,19 +1,11 @@
-export type Pizza = {
-  name: string;
-  ingredients: string[];
-  price: number;
-  extras: string[];
-};
+import {z} from 'zod';
+import {extraIngredientSchema, extraIngredientsSchema, pizzaSchema} from "./schemas";
 
-export type ExtraIngredients = Record<string, ExtraIngredient>;
-
-export type ExtraIngredient = {
-  name: string;
-  price: number;
-};
-
+export type Pizza = z.infer<typeof pizzaSchema>;
+export type ExtraIngredient = z.infer<typeof extraIngredientSchema>
+export type ExtraIngredients = z.infer<typeof extraIngredientsSchema>
 export type PizzaOnOrder = {
-  name: string;
-  price: number;
-  extraIngredients: ExtraIngredient[];
+    name: string;
+    price: number;
+    extraIngredients: ExtraIngredient[];
 };
